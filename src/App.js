2c34e621aaae86491450d81d99ba400700f7ac79
Tiffany <tiff21@uw.edu>
2020-02-18 23:05:08 -0800

    Initial Updates

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+
+import Header from './components/Header';
+import About from './components/About';
+import Work from './components/work';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <div className="App-header">
+      <Header></Header>
+      </div>
+      
+      <div className='About'>
+        <About></About>
+      </div>
+      <div className='work'>
+        <Work></Work>
+      </div>
+      <div className='contact'>
+        <Contact></Contact>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/yourdata.js
@@ -0,0 +1,74 @@
+export default
+    {
+        //(Please Do Not Remove The comma(,) after every variable)
+        //Change The Website Template
+        name :'tiffany truong.',
+        headerTagline: [//Line 1 For Header
+                        'Hi 👋 You found me! I am Tiffany Truong.',
+                        //Line 2 For Header
+                        'UX Designer',
+                        //Line 3 For Header
+                        'from Olympia, WA'
+    ],
+        //Contact Email
+        contactEmail:'tiff21@uw.edu',
+        // Add Your About Text Here
+        abouttext: "I am passionate about design for its unlimited possibility to impact people’s lives and the communities that I care about. With my design skill set and passion for technology, I have been able to research, create and design content to bring the Vietnamese community's needs and problems to light here in Seattle as well as educate the UW community about security awareness.",
+        aboutImage:'https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+       //Change This To Hide The Image of About Section (True Or False)
+       ShowAboutImage:true,//true or false (Change Here)
+       // Change Projects Here 
+       projects:[
+           {
+            id: 1,//DO NOT CHANGE THIS (Please)😅
+            title:'Student | Teacher | Employee | Manager', //Project Title - Add Your Project Title Here
+             service:'UI/UX Design', // Add Your Service Type Here
+            //Project Image - Add Your Project Image Here
+             imageSrc:"https://images.unsplash.com/photo-1558104631-0fa41a8f6c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+             //Project URL - Add Your Project Url Here
+             url:'http://chetanverma.com/'
+            },
+            {
+                id: 2,//DO NOT CHANGE THIS (Please)😅
+                title: 'Seattle Pinball Museum',
+                service: 'Brand Guidelines',
+                imageSrc: "https://images.unsplash.com/photo-1554866585-cd94860890b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                url: 'http://chetanverma.com/'
+            },
+            { 
+                id: 3,//DO NOT CHANGE THIS (Please)😅
+                title: 'Friends of Little Sàigòn',
+                service: 'Research & Advocacy Intern',
+                imageSrc: "https://images.unsplash.com/photo-1511500118080-275313ec90a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                url: 'http://chetanverma.com/'
+            },
+            {
+                id: 4,//DO NOT CHANGE THIS (Please)😅
+                title: 'Office of the Chief Information Security Officer',
+                service: 'Student Design Assistant',
+                imageSrc: "https://images.unsplash.com/photo-1558452919-08ae4aea8e29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                url: 'http://chetanverma.com/'
+           }
+
+                    /*
+
+                    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
+                ,{
+                id: 5,
+                title: 'Project Five',
+                service: 'Something Amazing',
+                imageSrc: "",
+                url: ''
+            }
+                */
+        ],
+        social: [
+            // Add Or Remove The Link Accordingly
+            {   name:'Github',
+                url:'https://github.com/tifftruong'},
+            {
+                name: 'Linkedin',
+                url: 'https://www.linkedin.com/in/tiffany-truong/'
+            }
+        ]
+    }
