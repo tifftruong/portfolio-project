d689b7dfcc1495a6644f7cc2f88d1a3cc82abd1d
Tiffany <tiff21@uw.edu>
2020-03-17 01:39:43 -0700

    Typist Animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import data from '../yourdata'
-
+import Typist from 'react-typist';
 
 class Header extends Component {
     state = {}
@@ -9,14 +9,17 @@ class Header extends Component {
     render() {
         return (<div>
             <header>
-                <Fade bottom cascade><img src={data.name} width="450" height="100" alt='brand logo'></img></Fade>
+                <Fade bottom cascade>
+                    <img src={data.name} width="450" height="100" alt='brand logo'></img>
+                </Fade>
             </header>
             <Fade bottom>
             <p className='header-title'>
-                {data.headerImg ? <img src={data.headerImg} width="600" height="300" alt='doodle' class='center'></img> : null}
+                    {data.headerImg ? <img src={data.headerImg} width="600" height="300" alt='doodle' class='center'></img> : null}
                 <br></br>
-                {data.headerTagline[0]}<br></br>{data.headerTagline[1]}
-               <br></br>
+                <Typist cursor={{hideWhenDone:true, show:true, blink:true, element:'|'}}>
+                    {data.headerTagline[0]}<br></br>{data.headerTagline[1]}
+                </Typist>
                     <button><a href={`mailto:${data.contactEmail}`} rel="noopener noreferrer">contact</a></button>
             </p>
             </Fade>
